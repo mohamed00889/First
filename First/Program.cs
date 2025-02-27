eb2b26344b0354016af9f4c8ad86d5db07a34a88
user <user@WIN-KTAKOJ2LS36>
2025-02-27 17:19:25 +0200

    Add project files.

---
+++ First/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace First
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number of small carpets:");
+            int SmallCarpets = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter number of Larg carpets:");
+            int LargCarpets = Convert.ToInt32(Console.ReadLine());
+            int cost = (SmallCarpets * 25) + (LargCarpets * 35);
+            double tax = cost * 0.06;
+            double total = cost + tax;
+            Console.WriteLine("The Cost =" + cost + "$");
+            Console.WriteLine("The Tax =" + tax + "$");
+            Console.WriteLine("The Total =" + total + "$");
+        }
+    }
+}
